0a30ab9ca078e315f60c6d851300c88ed79bc065
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2021-01-09 02:12:19 -0500

    works on ansible playbook

--- conanfile.py
+++ conanfile.py
@@ -1,18 +1,25 @@
 from conans import ConanFile, CMake, tools
 
+
 class HelloConan(ConanFile):
-    name = "hello"
+    name = "Hello"
     version = "0.1"
     license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
     url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of hello here>"
+    description = "<Description of Hello here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = {"shared": False}
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
     generators = "cmake"
 
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
     def source(self):
-        self.run("git clone https://github.com/hillagold/hello.git")
+        self.run("git clone https://github.com/conan-io/hello.git")
         # This small hack might be useful to guarantee proper /MT /MD linkage
         # in MSVC if the packaged project doesn't have variables to set it
         # properly
@@ -20,6 +27,16 @@ class HelloConan(ConanFile):
                               '''PROJECT(HelloWorld)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="hello")
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s'
+        #          % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
         self.copy("*.h", dst="include", src="hello")
@@ -31,3 +48,4 @@ conan_basic_setup()''')
 
     def package_info(self):
         self.cpp_info.libs = ["hello"]
+
